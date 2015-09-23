a076d13d4081e194965d86ab489dccf9ed1f4212
userg411 <userg411@gmail.com>
2015-09-23 17:40:05 +0500

    added test classes

---
+++ fx.java
@@ -0,0 +1,19 @@
+
+/**
+
+ * A sample that demonstrates a WebView object accessing a web page.
+
+ */
+import java.util.Arrays;	
+public class fx {
+
+ 
+
+
+    public static void main(String[] args) {
+
+        Arrays.asList( "a", "b", "d" ).forEach( e -> System.out.println( e ) );
+
+    }
+
+}
