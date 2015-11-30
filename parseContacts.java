f51ed7e063d65413115ed60d384ac5e9a28c2add
userg411 <userg411@gmail.com>
2015-12-01 00:06:20 +0500

    added parseContacts

---
+++ parseContacts.java
@@ -0,0 +1,62 @@
+import java.nio.file.Files;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class parseContacts{
+	private static ArrayList<Contact> contacts;
+	
+	public static void main(String args[]) throws Exception{
+		contacts = new ArrayList<Contact>();
+		List <String> lines  = Files.readAllLines(Paths.get("out.vcf"),Charset.defaultCharset());
+		for(int i=0; i<lines.size();){
+			String name = "";
+			String number = "";
+			if(lines.get(i).equals("BEGIN:VCARD")){
+				while(!lines.get(i).equals("END:VCARD")){
+					String[] details = lines.get(i).split(":");
+					if(details[0].equals("FN"))
+						name = details[1];
+					else if (details[0].equals("TEL;CELL;PREF")||details[0].equals("TEL;CELL"))
+						number = details[1];	
+					i++;
+				}
+			if(name.length()>0)	
+				contacts.add(new Contact(name, number));
+			
+			}
+			i++;
+		}
+		
+		ArrayList <Contact> result = new ArrayList<Contact>();
+		Iterator it = contacts.iterator();
+		while(it.hasNext()){
+			Contact c = (Contact)it.next();
+			if(!result.contains(c))
+				result.add(c);
+		}
+		Collections.sort(result);
+		for(Contact c:result)
+			System.out.println(c.name + "\t" + c.number);
+		System.out.println(result.size());
+		
+	}
+	static class Contact implements Comparable <Contact>{
+		private String name;
+		private String number;
+		public Contact(String name, String number){
+			this.name = name;
+			this.number = number;
+		}
+		@Override
+		public boolean equals(Object obj){
+			return (obj!=null && obj instanceof Contact && this.number.equals(((Contact)obj).number) );
+		}
+		@Override
+		public int compareTo(Contact con){
+			return this.name.compareTo(con.name);
+		}
+		
+	}
+}
